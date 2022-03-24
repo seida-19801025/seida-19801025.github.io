aa08cf65affb63396cf2d8449849534b72d7028f
Akihiro_Seida <100334205+seida-19801025@users.noreply.github.com>
2022-03-25 00:39:13 +0900

    Update program.js

--- program.js
+++ program.js
@@ -1,1 +1,28 @@
+const seCorrect = new Audio('mp3/correct.mp3');
 
+
+/**
+ * キーが押されたときのイベント
+ * @param {*} e 
+ * @returns 
+ */
+function eventKeyPress(e) {
+  se_off();
+  seCorrect.play();
+}
+
+/**
+ * 正解音を止める
+ * @returns 
+ */
+function seOff() {
+	// seCorrect.pause();
+	// seCorrect.currentTime = 0;
+	seCorrect.pause();
+	seCorrect.currentTime = 0;
+	return false;
+}
+
+
+
+document.addEventListener('keypress', eventKeyPress);
